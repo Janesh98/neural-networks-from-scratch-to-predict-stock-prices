2ba8235ea045a70e7f8aab63d219887e8785b766
Janesh Sharma <janeshk98@gmail.com>
2020-01-23 13:02:30 +0000

    increase hidden layer size

--- code/simplenn.py
+++ code/simplenn.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 class Neural_Network(object):
-    def __init__(self):        
+    def __init__(self):
         #Define Hyperparameters
         self.inputLayerSize = 2
         self.outputLayerSize = 1
-        self.hiddenLayerSize = 3
-        
+        self.hiddenLayerSize = 69
+
         #Weights (parameters)
         self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
         self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
-        
+
     def forward(self, X):
         #Propagate inputs though network
         self.z2 = np.dot(X, self.W1)
         self.a2 = self.sigmoid(self.z2)
         self.z3 = np.dot(self.a2, self.W2)
-        yHat = self.sigmoid(self.z3) 
+        yHat = self.sigmoid(self.z3)
         return yHat
-        
+
     def sigmoid(self, z):
         #Apply sigmoid activation function to scalar, vector, or matrix
         return 1/(1+np.exp(-z))
@@ -26,23 +26,23 @@ class Neural_Network(object):
     def sigmoidPrime(self,z):
         #Gradient of sigmoid
         return np.exp(-z)/((1+np.exp(-z))**2)
-    
+
     def costFunction(self, X, y):
         #Compute cost for given X,y, use weights already stored in class.
         self.yHat = self.forward(X)
         J = 0.5*sum((y-self.yHat)**2)
         return J
-        
+
     def costFunctionPrime(self, X, y):
         #Compute derivative with respect to W and W2 for a given X and y:
         self.yHat = self.forward(X)
-        
+
         delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
         dJdW2 = np.dot(self.a2.T, delta3)
-        
+
         delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
-        dJdW1 = np.dot(X.T, delta2)  
-        
+        dJdW1 = np.dot(X.T, delta2)
+
         return dJdW1, dJdW2
 
     #Helper Functions for interacting with other classes:
@@ -50,7 +50,7 @@ class Neural_Network(object):
         #Get W1 and W2 unrolled into vector:
         params = np.concatenate((self.W1.ravel(), self.W2.ravel()))
         return params
-    
+
     def setParams(self, params):
         #Set W1 and W2 using single paramater vector.
         W1_start = 0
@@ -58,7 +58,7 @@ class Neural_Network(object):
         self.W1 = np.reshape(params[W1_start:W1_end], (self.inputLayerSize , self.hiddenLayerSize))
         W2_end = W1_end + self.hiddenLayerSize*self.outputLayerSize
         self.W2 = np.reshape(params[W1_end:W2_end], (self.hiddenLayerSize, self.outputLayerSize))
-        
+
     def computeGradients(self, X, y):
         dJdW1, dJdW2 = self.costFunctionPrime(X, y)
         return np.concatenate((dJdW1.ravel(), dJdW2.ravel()))
@@ -74,7 +74,7 @@ def computeNumericalGradient(N, X, y):
             perturb[p] = e
             N.setParams(paramsInitial + perturb)
             loss2 = N.costFunction(X, y)
-            
+
             N.setParams(paramsInitial - perturb)
             loss1 = N.costFunction(X, y)
 
@@ -83,11 +83,11 @@ def computeNumericalGradient(N, X, y):
 
             #Return the value we changed to zero:
             perturb[p] = 0
-            
+
         #Return Params to original value:
         N.setParams(paramsInitial)
 
-        return numgrad 
+        return numgrad
 from scipy import optimize
 
 
@@ -95,17 +95,17 @@ class trainer(object):
     def __init__(self, N):
         #Make Local reference to network:
         self.N = N
-        
+
     def callbackF(self, params):
         self.N.setParams(params)
-        self.J.append(self.N.costFunction(self.X, self.y))   
-        
+        self.J.append(self.N.costFunction(self.X, self.y))
+
     def costFunctionWrapper(self, params, X, y):
         self.N.setParams(params)
         cost = self.N.costFunction(X, y)
         grad = self.N.computeGradients(X,y)
         return cost, grad
-        
+
     def train(self, X, y):
         #Make an internal variable for the callback function:
         self.X = X
@@ -113,7 +113,7 @@ class trainer(object):
 
         #Make empty list to store costs:
         self.J = []
-        
+
         params0 = self.N.getParams()
 
         options = {'maxiter': 200, 'disp' : True}
