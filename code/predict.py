993162636e1a89e6c370dc07d0b6bc4fff86cb80
Janesh Sharma <janeshk98@gmail.com>
2020-01-28 12:57:49 +0000

    measure accuracy of prediction

--- code/predict.py
+++ code/predict.py
@@ -3,6 +3,9 @@ from stock import get_stock_data
 import matplotlib.pyplot as plt
 from simplenn import Neural_Network, trainer
 
+def mse(a, b):
+    return (np.square(np.subtract(a, b)).mean()) / 1000
+
 def main():
     df = get_stock_data("TSLA")
     df = df['Adj Close']
@@ -22,8 +25,8 @@ def main():
     # Normalize
     X = X/np.amax(X, axis=0)
     y = y/1000 #make y less than 1
-    NN = Neural_Network()
 
+    NN = Neural_Network()
     T = trainer(NN)
     T.train(X,y)
     NN.costFunctionPrime(X,y)
@@ -32,6 +35,8 @@ def main():
     output *= 1000
     print(output)
 
+    print("\naccuracy: {:.4f}%".format(mse(y, output)))
+
     # plot actual price and prediction
     plt.plot(df[:100], "r")
     plt.plot(output, "b")
@@ -39,6 +44,7 @@ def main():
     plt.ylabel("Price")
     plt.title("Stock Prediction")
     plt.legend(["Actual", "Prediction"])
+    plt.grid()
     plt.show()
 
 if __name__ == "__main__":
