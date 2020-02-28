f0d6a778cc50b5b6b387949e746e00ad0bd79cb5
Janesh Sharma <janeshk98@gmail.com>
2020-02-28 18:53:12 +0000

    create seperate class for normalization

---
+++ code/normalize.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Normalize:
+    """ normalizes the data between 0 and 1
+        and reverts data back to original values """
+    def __init__(self, data):
+        self.factor = self.normalize_factor(data)
+
+    # finds a factor one bigger than the length of the data
+    # e.g.
+    # if max value in data is 100, factor is 1000
+    # if max value in data is 1500, factor is 10000
+    def normalize_factor(self, data):
+        max_val = round(max(data))
+        return int("1" + "0" * len(str(max_val)))
+
+    # scale data between 0 and 1
+    def normalize_data(self, data):
+        data = np.array(data, dtype=float)
+        return data / self.factor
+
+    # revert data back to original values
+    def denormalize_data(self, data):
+        return data * self.factor
