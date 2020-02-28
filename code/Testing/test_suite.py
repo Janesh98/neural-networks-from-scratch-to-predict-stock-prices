a3cb42530863d3f33bb0952c73d7b3663719a49f
Janesh Sharma <janeshk98@gmail.com>
2020-02-28 18:54:45 +0000

    normalization tests

--- code/Testing/test_suite.py
+++ code/Testing/test_suite.py
@@ -1,10 +1,11 @@
 import unittest
-from tests import ErrorTestCase, NeuralNetworksTestCase
+from tests import ErrorTestCase, NeuralNetworksTestCase, NormalizeTestCase
 
 def my_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(ErrorTestCase))
     suite.addTest(unittest.makeSuite(NeuralNetworksTestCase))
+    suite.addTest(unittest.makeSuite(NormalizeTestCase))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 
--- code/Testing/tests.py
+++ code/Testing/tests.py
@@ -6,6 +6,32 @@ sys.path.append('../')
 from NeuralNetwork import NeuralNetwork, mse, rmse, mape
 from RNN import RNN
 from lstm import LSTM
+from normalize import Normalize
+
+class NormalizeTestCase(unittest.TestCase):
+    """ ensure data is normalized between 0 and 1, 
+        then reverted back to the original values """
+    
+    def test_normalize(self):
+        test = np.arange(1000)
+
+        # normalize
+        scaler = Normalize(test)
+        normalized = scaler.normalize_data(test)
+
+        min_val = min(normalized)
+        max_val = max(normalized)
+
+        # ensure values scaled to range (0, 1)
+        self.assertGreaterEqual(min_val, 0.0)
+        self.assertLessEqual(max_val, 1.0)
+
+        # denormalize
+        denormalized = scaler.denormalize_data(normalized)
+
+        # ensure denormalized values are the same as the original
+        for x, y in zip(test, denormalized):
+            self.assertEqual(x, y)        
 
 class ErrorTestCase(unittest.TestCase):
     """ if both arrays are the same the error should be 0 """
