69f682bb74d3148ba8969909e711a327b3beed63
Paul McNally <paulmcnally@Pauls-MacBook-Air.local>
2020-03-05 17:33:12 +0000

    changes to formula

--- code/normalize.py
+++ code/normalize.py
@@ -5,15 +5,22 @@ class Normalize:
         and reverts data back to original values """
     def __init__(self, data):
         self.factor = self.normalize_factor(data)
+        self.minim = min(data)
 
     def normalize_factor(self, data):
-        return max(data)
+        return (max(data) - min(data))
 
     # scale data between 0 and 1
     def normalize_data(self, data):
         data = np.array(data, dtype=float)
+        for i in data:
+            i -= self.minim
         return np.divide(data, self.factor)
 
     # revert data back to original values
     def denormalize_data(self, data):
-        return np.dot(data, self.factor)
+        y = np.dot(data, self.factor)
+        print(self.minim)
+        for i in y:
+            i += self.minim
+        return y
