da9029e44cd23168fde9e4657744326f33fad5ba
Janesh Sharma <janeshk98@gmail.com>
2020-03-05 10:21:17 +0000

    move plotting function into stock.py and fix lines not connecting issue

--- code/stock.py
+++ code/stock.py
@@ -1,9 +1,9 @@
 from os import listdir
 import pandas as pd
 import datetime as dt
-from matplotlib import style
 import matplotlib.pyplot as plt
 import pandas_datareader.data as web
+import numpy as np
 
 def get_stock_data(ticker):
     csv = ticker + ".csv"
@@ -36,19 +36,24 @@ def get_stock_data(ticker):
             #print(df.tail())
             # save csv file
             df.to_csv("stock_data_csv/" + csv)
-
         
     return df
 
-def plot_stock(df):
-    df['Adj Close'].plot()
+def plot(actual, train, test):
+    #plt.plot([0 + i for i in range(0, 150)], actual, "r")
+    #plt.plot(prediction[:100], "b")
+    #plt.plot([99 + i for i in range(0, 51)], prediction[99:],  "g")
+
+    plt.plot(actual, label="Actual")
+    plt.plot(train, label="Train prediction")
+    test = [i for i in test]
+    # connect train and test lines
+    test.insert(0, train[-1])
+    # x values for test prediction plot
+    plt.plot([x for x in range(len(train)-1, len(train) + len(test)-1)], test, label="Test prediction")
+    plt.xlabel("Days")
+    plt.ylabel("Price")
+    plt.title("Stock Prediction")
+    plt.legend()
+    plt.grid()
     plt.show()
-
-def main():
-    style.use('ggplot')
-    ticker = "TSLA"
-    df = get_stock_data(ticker)
-    plot_stock(df)
-
-if __name__ == "__main__":
-    main()
