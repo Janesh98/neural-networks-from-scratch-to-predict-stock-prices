8f332ec379470360b589898cc182c6256df6565d
Janesh Sharma <janeshk98@gmail.com>
2020-03-05 08:46:39 +0000

    use numpys' funcs for div and mul as it's faster for large arrays

--- code/normalize.py
+++ code/normalize.py
@@ -6,18 +6,14 @@ class Normalize:
     def __init__(self, data):
         self.factor = self.normalize_factor(data)
 
-    # finds a factor one bigger than the length of the data
-    # e.g.
-    # if max value in data is 100, factor is 1000
-    # if max value in data is 1500, factor is 10000
     def normalize_factor(self, data):
         return max(data)
 
     # scale data between 0 and 1
     def normalize_data(self, data):
         data = np.array(data, dtype=float)
-        return data / self.factor
+        return np.divide(data, self.factor)
 
     # revert data back to original values
     def denormalize_data(self, data):
-        return data * self.factor
+        return np.dot(data, self.factor)
