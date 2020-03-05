c73ccf738603ea03760f1931ca5314eb7c8d0d18
Janesh Sharma <janeshk98@gmail.com>
2020-03-05 10:22:40 +0000

    remove uneccessary comments

--- code/stock.py
+++ code/stock.py
@@ -11,14 +11,12 @@ def get_stock_data(ticker):
         # load csv file if in current directory
         if csv in listdir(path=".stock_data_csv/"):
             df = pd.read_csv(".stock_data_csv/" + csv)
-            #print(df)
         
         # download csv from yahoo finance
         else:
             start = dt.datetime(2018, 12, 26)
             end = dt.datetime(2019, 12, 31)
             df = web.DataReader(ticker, 'yahoo', start, end)
-            #print(df.tail())
             # save csv file
             df.to_csv(".stock_data_csv/" + csv)
 
@@ -26,24 +24,18 @@ def get_stock_data(ticker):
         # load csv file if in current directory
         if csv in listdir(path="stock_data_csv/"):
             df = pd.read_csv("stock_data_csv/" + csv)
-            #print(df)
         
         # download csv from yahoo finance
         else:
             start = dt.datetime(2018, 12, 26)
             end = dt.datetime(2019, 12, 31)
             df = web.DataReader(ticker, 'yahoo', start, end)
-            #print(df.tail())
             # save csv file
             df.to_csv("stock_data_csv/" + csv)
         
     return df
 
 def plot(actual, train, test):
-    #plt.plot([0 + i for i in range(0, 150)], actual, "r")
-    #plt.plot(prediction[:100], "b")
-    #plt.plot([99 + i for i in range(0, 51)], prediction[99:],  "g")
-
     plt.plot(actual, label="Actual")
     plt.plot(train, label="Train prediction")
     test = [i for i in test]
